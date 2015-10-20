62cc2974c7f57384a66f75dae2d1a3513440dde3
zhenhappy <505507538@qq.com>
2015-10-20 21:33:35 +0800

    表单与事件

--- App.jsx
+++ App.jsx
@@ -2,17 +2,15 @@
 // App component - represents the whole app
 App = React.createClass({
 
-  // 修改内容起始
   // 这个mixin使得getMeteorData方法可以使用
   mixins: [ReactMeteorData],
 
   // 从Tasks集合中获取数据并添加到this.data中
   getMeteorData() {
     return {
-      tasks: Tasks.find({}).fetch()
+      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
     }
   },
-  // 修改内容结束
 
   renderTasks() {
     // 从this.data中获取数据
@@ -21,11 +19,36 @@ App = React.createClass({
     });
   },
 
+  // 监听表单的回车事件
+  handleSubmit(event) {
+    // 取消事件的默认动作
+    event.preventDefault();
+
+    // 通过React的ref属性找到输入框的值
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+    console.log(text);
+
+    // 插入到数据库
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // 当前时间
+    });
+
+    // 复原表单
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>
