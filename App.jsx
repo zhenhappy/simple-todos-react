7d046a288feb9cdcd4b7ddc8cfa47f87f52fc457
zhenhappy <505507538@qq.com>
2015-10-20 21:07:29 +0800

    集合

--- App.jsx
+++ App.jsx
@@ -1,15 +1,22 @@
 // 不要在App前面添加 var
+// App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+
+  // 修改内容起始
+  // 这个mixin使得getMeteorData方法可以使用
+  mixins: [ReactMeteorData],
+
+  // 从Tasks集合中获取数据并添加到this.data中
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
   },
+  // 修改内容结束
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    // 从this.data中获取数据
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
@@ -28,3 +35,5 @@ App = React.createClass({
     );
   }
 });
+
+
--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,12 +1,18 @@
+//前后端都会执行的代码块
+console.log("Hello,Meteor!");
+// 定义一个集合来支撑我们的任务列表
+Tasks = new Mongo.Collection("tasks");
+
 if(Meteor.isClient){
   //仅在前端执行的代码块
   Meteor.startup(function () {
     // 在页面加载完成之后，使用 Meteor.startup 来渲染 React 组件
     React.render(<App />, document.getElementById("render-target"));
   });
-}else if(Meteor.isServer){
-  //仅在后端执行的代码块
-}else{
-  //前后端都会执行的代码块
-  console.log("Hello,Meteor!");
 }
+
+if(Meteor.isServer){
+  //仅在后端执行的代码块
+}
+
+
